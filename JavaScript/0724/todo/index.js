e23521cde8b06c15d689d1c3dd878131e23848e6
page1597 <gimm08377@gmail.com>
2024-07-24 15:36:39 +0900

    Add: 0724 투두리스트 live server 없어도 동작되도록 수정

--- JavaScript/0724/todo/index.js
+++ JavaScript/0724/todo/index.js
@@ -2,16 +2,15 @@ const $input = document.querySelector("input");
 const $button = document.querySelector("button");
 const $ul = document.querySelector("ul");
 const $form = document.querySelector("form");
-
+// 새로고침 되는 이유 -> live server 때문
 const fetchTodos = async function () {
   const response = await fetch("http://localhost:3000/todos");
   const data = await response.json();
   return data;
 };
-let todos = [];
 
 const loadTodos = async function () {
-  todos = await fetchTodos();
+  let todos = await fetchTodos();
   console.log(todos);
 
   todos.forEach((todo) => {
@@ -22,27 +21,32 @@ const loadTodos = async function () {
 };
 loadTodos();
 
-const addTodo = function () {
+const addTodo = async function () {
   const inputValue = $input.value;
   // 실제 db json에 추가하기
   const newTodo = {
     todo: inputValue,
     done: false,
   };
-  fetch("http://localhost:3000/todos", {
+  const data = await fetch("http://localhost:3000/todos", {
     method: "POST",
     headers: {
       "Content-type": "application/json",
     },
     body: JSON.stringify(newTodo),
   });
+  const newPost = await data.json();
+  const $li = document.createElement("li");
+  $li.textContent = newPost.todo;
+  $ul.appendChild($li);
+  $input.value = "";
 };
 
 // $button.addEventListener("click", () => {
 //   addTodo();
 // });
 
-$form.addEventListener("submit", function (e) {
+$form.addEventListener("submit", async function (e) {
   e.preventDefault();
-  addTodo();
+  await addTodo();
 });
