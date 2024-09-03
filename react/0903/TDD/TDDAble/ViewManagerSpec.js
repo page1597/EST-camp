7acb15fdc83c94f2e9044bcae303a07e4bd60ebd
page1597 <gimm08377@gmail.com>
2024-09-03 16:12:56 +0900

    Add: 0903 TDD 실습
    - TDD와 Jest

---
+++ react/0903/TDD/TDDAble/ViewManagerSpec.js
@@ -0,0 +1,52 @@
+describe("클릭 이벤트 처리 및 뷰를 담당하는 함수입니다.", () => {
+  it("viewManager에 textManager가 잘 전달되는지 확인합니다.", () => {
+    const textManager = null;
+    const elements = {
+      btnEl: document.createElement("button"),
+      viewerEl: document.createElement("div"),
+      inpEl: document.createAttribute("input"),
+    };
+
+    const actual = () => new ViewManager(textManager, elements);
+
+    // toThrowError를 사용하여 에러가 발생하는지 확인합니다.
+    expect(actual).toThrowError();
+  });
+
+  it("viewManager에 필요한 요소 세가지가 잘 전달되는지 확인합니다.", () => {
+    const textManager = new TextManager();
+    const elements = {
+      btnEl: null,
+      viewerEl: null,
+      inpEl: null,
+    };
+
+    const actual = () => new ViewManager(textManager, elements);
+
+    // toThrowError를 사용하여 에러가 발생하는지 확인합니다.
+    expect(actual).toThrowError();
+  });
+
+  const textManager = new TextManager();
+  const elements = {
+    btnEl: document.createElement("button"),
+    viewerEl: document.createElement("div"),
+    inpEl: document.createAttribute("input"),
+  };
+  const viewManager = new ViewManager(textManager, elements);
+
+  it("click 이벤트가 발생했을 때 changeValue 함수가 호출되는지 확인합니다.", () => {
+    // 특정 객체의 함수를 감시합니다.
+    spyOn(viewManager, "changeValue");
+    elements.btnEl.click();
+
+    // toHaveBeenCalled: 함수의 호출을 확인
+    expect(viewManager.changeValue).toHaveBeenCalled();
+  });
+  it("changeValue 함수가 호출되었을 때, updateView 함수가 호출되는지 확인합니다.", () => {
+    spyOn(viewManager, "updateView");
+    viewManager.changeValue();
+
+    expect(viewManager.updateView).toHaveBeenCalled();
+  });
+});
---
+++ react/0903/TDD/color-button/src/App.js
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [buttonColor, setButtonColor] = useState("red");
+  const newColor = buttonColor === "red" ? "blue" : "red";
+
+  return (
+    <button
+      role="link"
+      style={{ backgroundColor: buttonColor }}
+      onClick={() => {
+        setButtonColor(newColor);
+      }}
+    >
+      change to {newColor}!
+    </button>
+  );
+}
+
+export default App;
