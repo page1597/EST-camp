be789f16eb61d11a233d237b4b29bc61bed1eca5
page1597 <gimm08377@gmail.com>
2024-09-11 16:10:14 +0900

    Add: 0911 Scss 실습

---
+++ scss/0911/react-scss-example/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React와 SCSS 예제</h1>
+      <div className="content">
+        <p>이것은 SCSS를 사용한 스타일링 예제입니다.</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
